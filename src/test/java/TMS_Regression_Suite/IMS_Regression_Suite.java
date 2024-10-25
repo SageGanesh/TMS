57e49ce0ce464ca68da72cb2f259a1f2a953d86e
SageMukhid <mpathan@sageitinc.com>
2024-10-25 12:03:02 +0530

    New VMS code commit

---
+++ src/test/java/TMS_Regression_Suite/IMS_Regression_Suite.java
@@ -0,0 +1,31 @@
+package TMS_Regression_Suite;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class IMS_Regression_Suite extends Base_Page_TMS {
+
+	WebDriverWait wait;
+
+	ExtentReports extent = new ExtentReports();
+	ExtentSparkReporter spark = new ExtentSparkReporter("Html_Report/Extentreport1.html");
+
+	@BeforeTest
+	public void Report_setup() {
+		spark.config().setDocumentTitle("html_report");
+		spark.config().setTheme(Theme.STANDARD);
+		extent.attachReporter(spark);
+
+	}
+
+	@AfterTest
+	public void report_flush() {
+		extent.flush();
+
+	}
+}
