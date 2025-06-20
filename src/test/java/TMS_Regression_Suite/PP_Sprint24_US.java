cd9f3b6b7e8ef0acbab9fd2f6c008ceaa129f962
SageMukhid <mpathan@sageitinc.com>
2025-06-20 11:54:39 +0530

    Admin Panel - Work Authorization scenario.

--- src/test/java/TMS_Regression_Suite/PP_Sprint24_US.java
+++ src/test/java/TMS_Regression_Suite/PP_Sprint24_US.java
@@ -2,12 +2,14 @@ package TMS_Regression_Suite;
 
 import java.awt.AWTException;
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -117,5 +119,110 @@ public class PP_Sprint24_US extends Base_Page_TMS {
 		log.pass("Successfully Close the browser");
 		log.pass("Successfully Add/Update End Client");
 
+	}
+	
+	/*
+	 * Add New Work Authorization by Admin Panel: This method is use to add new Work
+	   Authorization and verify it is coming in Applicant WA drop down.
+	 * 
+	 * Steps: 
+	 * 1. Open the Browser 
+	 * 2. Login the application(Admin Role)
+	 * 3. Successfully open the Dashboard page 
+	 * 4. Click on Admin Settings 
+	 * 5. Click on Work Authorizations
+	 * 6. Click on Add New button
+	 * 7. Input Work Authorization Name
+	 * 8. Confirmation Pop-up came
+	 * 9. Click on Applicants icon 
+	 *10. Click on Add New button 
+	 *11. Verify in WA dropdown, created WA is there or not
+	 *12. Close the browser
+	 * 
+	 */
+	
+	@Test(priority = 2, description = "Add new Work Authorization using Admin Panel")
+	public void AdminPanel_AddWorkAuthorization() throws InterruptedException, AWTException {
+
+		ExtentTest log = extent.createTest("Add new Work Authorization using Admin Panel")
+				.assignCategory("Add Work Authorization using Admin Panel");
+		System.out.println(log.getStatus());
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@root.com");
+		driver.findElement(By.xpath("//input[@placeholder='Enter password']")).sendKeys("123Pa$$word!");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(8000);
+		log.info("Login with valid credentials");
+		log.pass("Successfully login the application");
+
+		WebElement ele1 = driver.findElement(By.xpath("//i[@class='fa fa-cogs ng-star-inserted']"));
+		ele1.click();
+		Thread.sleep(3000);
+		log.info("Click on Admin Settings Icon");
+		log.pass("Successfully click on Admin Settings Icon");
+
+		// driver.findElement(By.xpath("//a[normalize-space()='Work
+		// Authorizations']")).click();
+		WebElement element = driver.findElement(By.xpath("//a[normalize-space()='Work Authorizations']"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+		element.click();
+		Thread.sleep(5000);
+		log.info("Click on Add New button");
+		log.pass("Successfully click on Add New button");
+
+		driver.findElement(By.xpath("//a[normalize-space()='Add New']")).click();
+		Thread.sleep(2000);
+		log.info("Click on Add New button");
+		log.pass("Successfully click on Add New button");
+
+		driver.findElement(By.xpath("//input[@id='form-name']")).sendKeys("Automation WA03");
+		Thread.sleep(2000);
+		log.info("Input Work Authorization Name in textbox");
+		log.pass("Successfully input the Work Authorization Name in textbox");
+
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(2000);
+		log.info("Click on Save button");
+		log.pass("Successfully Click on Save button");
+
+		WebElement element1 = driver.findElement(By.xpath("//a[@href='/applicants']"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element1);
+		element1.click();
+		Thread.sleep(5000);
+		log.info("Click on Applicants Icon");
+		log.pass("Successfully click on Applicants Icon");
+
+		driver.findElement(By.xpath("//a[contains(text(),'Add New')]")).click();
+		Thread.sleep(2000);
+		log.info("Click on Add New button");
+		log.pass("Successfully click on Add New button");
+
+		By dropdownLocator = By.xpath("//select[@id='form-workAuthorization']");
+		String expectedOption = "Automation WA03";
+
+		// Verify this field is present in QA drop down or not.
+		WebElement dropdownElement = driver.findElement(dropdownLocator);
+		Select dropdown = new Select(dropdownElement);
+		List<WebElement> options = dropdown.getOptions();
+
+		boolean optionFound = false;
+		for (WebElement option : options) {
+			if (option.getText().trim().equalsIgnoreCase(expectedOption.trim())) {
+				optionFound = true;
+				break;
+			}
+		}
+
+		// Assert and log result
+		if (optionFound) {
+			log.pass(expectedOption + "' is present in the Work Authorization dropdown.");
+		} else {
+			log.fail(expectedOption + "' is NOT present in the Work Authorization dropdown.");
+			Assert.fail(expectedOption + "' is not present in the Work Authorization dropdown.");
+		}
+
+		driver.close();
+		log.pass("Successfully Close the browser");
+		log.pass("Successfully Add/Update End Client");
 	}
 }
